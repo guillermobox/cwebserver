4f6bf6cff3dd8694a11bf8875969d3f0aec00987
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2013-07-17 18:23:29 +0200

    Fixed coding style

--- server.c
+++ server.c
@@ -14,156 +14,164 @@
 
 int sockfd=-1;
 
-void error (char *str) {
-	perror (str);
-	exit (EXIT_FAILURE);
+void error(char *str)
+{
+	perror(str);
+	exit(EXIT_FAILURE);
 }
 
 #define BUFFLEN 256
-void copy (char *filepath, int fdout) {
-	char * buffer, headers[128];
+void copy(char *filepath, int fdout)
+{
+	char *buffer, headers[128];
 	struct stat st;
 	int fd;
 	time_t t;
 	ssize_t nchars;
 
-	fd = open (filepath, O_RDONLY);
+	fd = open(filepath, O_RDONLY);
 	if (fd < 0) {
-		perror ("opening file");
+		perror("opening file");
 		exit(EXIT_FAILURE);
 	}
 
-	buffer = (char*)malloc(BUFFLEN*sizeof(char));
+	buffer = (char*)malloc(BUFFLEN * sizeof(char));
 
-	stat (filepath, &st);
+	stat(filepath, &st);
 
-	t = time (NULL);
+	t = time(NULL);
 
-	bzero (headers, 128);
-	strcpy (headers, "HTTP/1.1 200 OK\n");
-	if( strncmp(filepath+strlen(filepath)-5, ".html", 5)==0 )
-		strcat (headers, "Content-Type: text/html\n");
+	bzero(headers, 128);
+	strcpy(headers, "HTTP/1.1 200 OK\n");
+	if (strncmp(filepath + strlen(filepath) - 5, ".html", 5)==0)
+		strcat(headers, "Content-Type: text/html\n");
 	else
-		strcat (headers, "Content-Type: text/plain\n");
-	sprintf (headers + strlen (headers), "Content-length: %ld\n", st.st_size);
-	strcat (headers, "Date: ");
-	strcat (headers, ctime (&t));
-	strcat (headers, "\n");
+		strcat(headers, "Content-Type: text/plain\n");
+	sprintf(headers + strlen(headers), "Content-length: %ld\n", st.st_size);
+	strcat(headers, "Date: ");
+	strcat(headers, ctime(&t));
+	strcat(headers, "\n");
 
-	write (fdout, headers, strlen (headers));
+	write(fdout, headers, strlen(headers));
 
 	while (1) {
-		nchars = read (fd, buffer, BUFFLEN);
+		nchars = read(fd, buffer, BUFFLEN);
 		if (nchars == 0) {
 			break;
-		}else if (nchars < 0) {
+		} else if (nchars < 0) {
 			perror ("reading file");
 			exit (1);
 		} else {
 			write (fdout, buffer, nchars);
 		}
 	}
-	close (fd);
+	close(fd);
 }
 
-char * geturl (char *header) {
+char *geturl(char *header)
+{
 	char *ptstart, *ptend;
 	char *ret;
 
-	ptstart = index (header, ' ') + 2;
-	ptend = index (ptstart, ' ');
+	ptstart = index(header, ' ') + 2;
+	ptend = index(ptstart, ' ');
 
-	ret = calloc (128, sizeof (char));
-	strncpy (ret, ptstart, ptend - ptstart);
-	ret[strlen (ret)] = '\0';
+	ret = calloc(128, sizeof(char));
+	strncpy(ret, ptstart, ptend - ptstart);
+	ret[strlen(ret)] = '\0';
 	return ret;
 }
 
-int handle (int newsockfd) {
+int handle(int newsockfd)
+{
 	char buffer[256], *path;
 	int n;
 
-	bzero (buffer, 256);
-	n = read (newsockfd, buffer, 255);
+	bzero(buffer, 256);
+	n = read(newsockfd, buffer, 255);
 	if (n < 0)
-		error ("error reading");
+		error("error reading");
 
-	path = geturl (buffer);
-	if( strlen(path)==0 ) puts(buffer);
+	path = geturl(buffer);
+	if (strlen(path)==0)
+		puts(buffer);
 	puts(path);
 
 	while (n == 255)
-		n = read (newsockfd, buffer, 255);
+		n = read(newsockfd, buffer, 255);
 
-	if (access (path, R_OK) != 0)
-		write (newsockfd, "HTTP/1.1 404 Not found\n", 23);
+	if (access(path, R_OK) != 0)
+		write(newsockfd, "HTTP/1.1 404 Not found\n", 23);
 	else
-		copy (path, newsockfd);
+		copy(path, newsockfd);
 
-	close (newsockfd);
-	free (path);
+	close(newsockfd);
+	free(path);
 }
 
-void waitforit(int sig){
+void waitforit(int sig)
+{
 	int a;
 	wait(&a);
 }
 
-void abandonship(int sig){
+void abandonship(int sig)
+{
 	printf("[%d childof %d] %s\n", getpid(), getppid(), "KILLSIGNAL received, quitting...");
 	fflush(stdout);
-	if( sockfd!=-1 ) close(sockfd);
+	if (sockfd!=-1)
+		close(sockfd);
 	sockfd = -1;
 }
 	
-int main (int argc, char *argv[]) {
-	int  newsockfd, portno, pid;
+int main (int argc, char *argv[])
+{
+	int newsockfd, portno, pid;
 	socklen_t clilen;
 	char buffer[256];
 
 	struct sockaddr_in serv_addr, cli_addr;
 	int err;
 
-	signal (SIGINT,  &abandonship);
-	signal (SIGCHLD, &waitforit);
+	signal(SIGINT,  &abandonship);
+	signal(SIGCHLD, &waitforit);
 	portno = 8080;
 
-	sockfd = socket (AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
-		error ("error socketing");
+		error("error socketing");
 
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons (portno);
+	serv_addr.sin_port = htons(portno);
 
-	err = bind (sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr));
+	err = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
 	if (err < 0)
-		error ("error binding\n");
+		error("error binding\n");
 
-	listen (sockfd, 5);
-	clilen = sizeof (cli_addr);
+	listen(sockfd, 5);
+	clilen = sizeof(cli_addr);
 
 	while (1) {
-
-		newsockfd = accept (sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 		if (newsockfd < 0)
 			error ("error accepting");
 
-		switch( fork() ){
-			case -1:
-				error("error forking\n");
-				break;
-			case 0:
-				close (sockfd);
-				handle (newsockfd);
-				exit (EXIT_SUCCESS);
-				break;
-			default:
-				close (newsockfd);
+		switch (fork()) {
+		case -1:
+			error("error forking\n");
+			break;
+		case 0:
+			close(sockfd);
+			handle(newsockfd);
+			exit(EXIT_SUCCESS);
+			break;
+		default:
+			close(newsockfd);
 		}
 	}
 
-	close (sockfd);
+	close(sockfd);
 
 	return 0;
 }
