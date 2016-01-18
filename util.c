8697399093e684c2f6cc96db7c8a065b427de1e6
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-01-18 23:19:51 +0100

    Implement stringf to print growing strings

--- util.c
+++ util.c
@@ -2,6 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include "util.h"
+
+char *stringf(struct string *s, const char *fmt, ...)
+{
+	va_list args;
+	size_t size;
+
+	if (!s)
+		return NULL;
+
+	va_start(args, fmt);
+	size = vsnprintf(NULL, 0, fmt, args);
+	va_end(args);
+
+	if (size < 0)
+		return NULL;
+
+	if (s->content) {
+		if (s->allocated < s->length + size + 1) {
+			while (s->allocated < s->length + size + 1) {
+				s->allocated *= 2;
+			}
+			s->content = realloc(s->content, s->allocated);
+			if (!s->content)
+				return NULL;
+		}
+	}
+	else {
+		s->allocated = size + 1;
+		s->content = malloc(s->allocated);
+		if (!s->content)
+			return NULL;
+		s->length = 0;
+	}
+
+	va_start(args, fmt);
+	vsnprintf(s->content + s->length, size + 1, fmt, args);
+	va_end(args);
+
+	s->length += size;
+	return s->content;
+};
 
 void error(char *str)
 {
@@ -18,4 +60,3 @@ void info(const char *format, ...)
 	va_end(args);
 	putchar('\n');
 }
-
--- util.h
+++ util.h
@@ -1,3 +1,16 @@
 
 void error(char *str);
 void info(const char *format, ...);
+
+/*
+ * string manipulation routine (stringf)
+ */
+struct string {
+	char *content;
+	size_t allocated;
+	size_t length;
+};
+
+struct string STRING_EMPTY = {NULL, 0, 0};
+
+char *stringf(struct string *s, const char *fmt, ...);
